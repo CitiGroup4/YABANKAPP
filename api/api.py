2959685aa647d1971e3c3b756171e572e7220b62
Pranav Balu <pranavbalu1@outlook.com>
2026-07-27 13:23:02 -0500

    add routes on main api file

--- api/api.py
+++ api/api.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/users/register")
+def register_user():
+    pass
+
+@app.get("/users/view")
+def view_user():
+    pass

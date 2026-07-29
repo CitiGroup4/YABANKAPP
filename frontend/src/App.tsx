4a68668095b545e0ff882043c772976d949fc3a3
Pranav Balu <pranavbalu1@outlook.com>
2026-07-29 16:48:14 -0500

    add login/register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
+import Auth from "./pages/Auth.tsx";
+
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        YABank Frontend
-      </h1>
-    </div>
-  );
+  return <Auth />;
 }
 
 export default App;
